0d646228c2502e81d6b174287a86ec4efeb8061f
Hamida Sh <sharifovahamida@gmail.com>
2021-05-23 13:34:30 -0700

    MVP tasks

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,36 @@
+const accountsDB = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  const accounts = accountsDB("accounts");
+  return accounts;
+};
 
-const getById = id => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  const account = accountsDB("accounts").where({id});
+  return account;
+};
 
-const create = account => {
+const create = async (account) => {
   // DO YOUR MAGIC
-}
+  const newAccount = accountsDB("accounts").insert(account);
+  
+  return newAccount;
+};
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
-}
+  const updatedAccount = accountsDB("accounts").update(account).where({id});
+  return updatedAccount;
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // DO YOUR MAGIC
-}
+  const deletedAcct = accountsDB("accounts").del().where({id})
+  
+  return deletedAcct;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +38,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,90 @@
 const router = require('express').Router()
+const accountsDB = require('./accounts-model')
+const accountsMiddleware = require('./accounts-middleware')
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  accountsDB.getAll()
+  .then(result => {
+    res.status(200).json({data: result});
+  })
+  .catch(err => {
+    res.status(500).json({message: `Failed to get all the accounts`})
+  })
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', accountsMiddleware.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params
+  // console.log(id);
+  accountsDB
+  .getById(id)
+  .then(result => {
+    res.status(200).json({data: result})
+  })
+  .catch(err => {
+    res.status(500).json({message: "Failed to  get the requested account id"})
+  })
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  const newAccount = req.body;
+  // console.log(newAccount);
+
+  accountsDB
+  .create(newAccount)
+  .then(resultId => {
+    accountsDB.getById(resultId[0])
+    .then(result => {
+      res.status(200).json({data: result})
+    })
+    .catch(err => {
+      res.status(500).json({message: `Failed to retrieve created account id ${resultId[0]}`})
+    })
+    // res.status(200).json({data: resultId});
+  })
+  .catch(err => {
+    res.status(500).json({message: `Failed to create a new account`})
+  })
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  const updateAccount = req.body;
+  const {id} = req.params;
+
+  accountsDB.updateById(id, updateAccount)
+  .then(result => {
+    if(result === 0){
+      res.status(404).json({message: `No records were found to be updated`})
+    }
+    else{
+      res.status(200).json({message: `Account id ${id} succcessfully updated`})
+    }
+  })
+  .catch(err => {
+       res.status(500).json({message: `Failed to update id ${id}`})
+  })
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params
+  accountsDB.deleteById(id)
+  .then(result => {
+    res.status(200).json({message: `Account id ${id} deleted successfully`})
+  })
+  .catch(err => res.status(500).json({message: `Failed to deleted acount id ${id}`}))
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(500).json({
+    message: `failed to route in accounts router`,
+    error: err.message,
+  })
+  next()
 })
 
 module.exports = router;
